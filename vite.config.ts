4b3bff1a38b5206adf5219399fa2c66795103ddd
Walid Ashraf <walidashraf050@gmail.com>
2025-08-26 23:35:32 +0300

    Project Donee 🤍✔

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITEBASE_PATH || "/Social-Media-App-Supabase",
 })
