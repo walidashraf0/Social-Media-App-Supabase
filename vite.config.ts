e2e4081d91d2206088711393e2c9ab01dd54c2fc
Walid Ashraf <walidashraf050@gmail.com>
2025-08-27 12:56:45 +0300

    Edit on vercel.json

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITEBASE_PATH || "/Social-Media-App-Supabase",
+  base: '/',
   build: {
     outDir: 'dist'
   }
